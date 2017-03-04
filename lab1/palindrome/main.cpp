3bfd4e6c8aab63014fbbcd24ada87ec5f4e0c5d4
Wilkkor <kornelwil@gmail.com>
2017-03-04 13:03:38 +0100

    Update main.cpp

--- lab1/palindrome/main.cpp
+++ lab1/palindrome/main.cpp
@@ -7,14 +7,27 @@
 int main()
 {
     std::string str;
-    std::cin>>str;
-    if(is_palindrome(str)==1)
+
+    bool r=1;
+    while(r==1)
     {
-        std::cout<<"tak";
+        std::cout<<"by sprawdzic czy palindrom podaj 1 by wyjsc podaj 0";
+        std::cin>>r;
+        if(r==1)
+        {
+            std::cout<<"podaj wyraz";
+            std::cin>>str;
+            if(is_palindrome(str)==1)
+            {
+                std::cout<<"tak";
+            }
+            else
+            {
+                std::cout<<"nie";
+            }
+            return 0;
+        }
     }
-    else
-    {
-        std::cout<<"nie";
-    }
-    return 0;
+
+
 }
